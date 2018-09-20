f3a5434088a4eeb59a69104b7dec2c756af2ee63
AhmadMaZulPahwan7 <38656579+ahmadmazulpahwan7@users.noreply.github.com>
2018-09-20 09:39:32 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,60 @@
+#Install script By Ubaii ID
+#hargai pembuat. jangan otak Atik script nya.
+#makasih atas kerjasama nya
+
+#Package Termux
+pkg update && pkg upgrade -y
+pkg install lynx -y
+pkg install python2 -y
+pkg install figlet -y
+pkg install ruby -y
+pkg install php -y
+pkg install nano -y
+pkg install bash -y
+pkg install w3m -y
+apt update && apt upgrade -y
+apt install python2 -y
+pip2 install rsa -y
+apt-get install tor -y
+pkg install gnupg -y
+pkg install php python -y
+pkg install python python2 vim figlet curl -y
+pkg install bash -y
+pip2 install bash -y
+apt install nano -y
+pkg install curl -y
+apt install openssl curl -y
+pkg install libcurl -y
+pkg install termcolor -y
+pip2 install termcolor -y
+apt install php -y
+apt-get install screenfetch -y
+pip2 install requests -y
+pip2 install lolcat -y
+apt install cowsay -y
+apt install ruby -y
+pkg install clang -y
+pkg install gem -y
+gem install lolcat -y
+apt install git -y
+pkg install git php -y
+pkg install git python2 -y
+apt install figlet -y
+pkg install clang -y
+pkg install sh -y
+apt-get install wget -y
+
+#Install Gems
+gem install lolcat
+
+#intro
+echo AhmadMaZulPahwan7
+echo //////////////////////////
+echo //        Termux tools //
+echo //   Muslim Cyber Army//
+echo //////////////////////////
+figlet Sukses | lolcat
+figlet Install | lolcat
+figlet Xshell | lolcat
+clear
+sh ahmad.sh
